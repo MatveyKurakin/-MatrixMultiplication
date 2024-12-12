3060bb72ae4f1371658b28b773881eafd0c1e911
Matvej Kurakin <matvejkurakin7755@gmail.com>
2024-12-12 21:06:17 +0300

    use intrinsic

--- matrix_multiplication.cpp
+++ matrix_multiplication.cpp
@@ -23,37 +23,33 @@ void generation(double* mat, size_t size)
 void matrix_mult(double* a, double* b, double* res, size_t size)
 {
 	int size_block_x = 4;
-	int size_block_y = 16;
+	int size_block_y = 4;
 	int isize = size;
 #pragma omp parallel for
 	for (int ib = 0; ib < isize; ib += size_block_y) {
 		for (int kb = 0; kb < isize; kb += size_block_x) {
-			for (int jb = 0; jb < isize; jb += size_block_y) {
-				int iEnd = std::min(isize, ib + size_block_y);
-				int jEnd = std::min(isize, jb + size_block_y);
-				int kEnd = kb + size_block_x;
-				for (int i = ib; i < iEnd; i++) {
+			int iEnd = ib + size_block_y;
+			int kEnd = kb + size_block_x;
+			for (int i = ib; i < iEnd; i++) {
+#pragma omp unroll
+				for (int k = kb; k < kEnd; k++){
 					double* iRes = res + i * isize;
-					double* iB_0 = b + kb * isize;
-					double* iB_1 = b + (kb + 1) * isize;
-					double* iB_2 = b + (kb + 2) * isize;
-					double* iB_3 = b + (kb + 3) * isize;
-					double iA_0 = a[i * isize + kb];
-					double iA_1 = a[i * isize + kb + 1];
-					double iA_2 = a[i * isize + kb + 2];
-					double iA_3 = a[i * isize + kb + 3];
+					double* iB = b + k * isize;
+					__m256 ra, rb, rres;
+					ra = _mm256_set1_pd(a[i * isize + k]);
 #pragma omp simd
-					for (int j = jb; j < jEnd; j++) {
-						iRes[j] += iA_0 * iB_0[j];
-						iRes[j] += iA_1 * iB_1[j];
-						iRes[j] += iA_2 * iB_2[j];
-						iRes[j] += iA_3 * iB_3[j];
+					for (int j = 0; j < isize - 3; j += size_block_y) {
+						rres = _mm256_loadu_pd(iRes + j);
+						rb = _mm256_loadu_pd(iB + j);
+						rres = _mm256_fmadd_pd(ra, rb, rres);
+						_mm256_storeu_pd(iRes + j, rres);
 					}
-				}
+				}				
 			}
 		}
 	}
 }
+
 
 int main()
 {
